34aa8cf5acd35de01282ec855ad60f85f45f50e8
Magicoff <danilka.3012@yandex.ru>
2024-09-16 15:16:04 +0300

    mega work

---
+++ main-service/src/main/java/ru/practicum/Constants.java
@@ -0,0 +1,29 @@
+package ru.practicum;
+
+
+public class Constants {
+
+    public static final String COMPILATION_PATH_ID = "/{compId}";
+
+    public static final String ID = "/{id}";
+
+    public static final String EVENT_PATH_ID = "/{eventId}";
+
+    public static final String USER_PATH_ID = "/{userId}";
+
+    public static final String EVENTS_PATH_ID = "/events/{eventId}";
+
+    public static final String REQUEST_PATH_ID = "/{requestId}";
+
+    public static final String REQUESTS_PATH = "/requests";
+
+    public static final String EVENTS_PATH = "/events";
+
+    public static final String USER_EVENT_REQUESTS_PATH = "/{userId}/events/{eventId}/requests";
+
+
+
+
+
+
+}
---
+++ main-service/src/main/java/ru/practicum/model/event/AdminStateAction.java
@@ -0,0 +1,5 @@
+package ru.practicum.model.event;
+
+public enum AdminStateAction {
+    PUBLISH_EVENT, REJECT_EVENT
+}
